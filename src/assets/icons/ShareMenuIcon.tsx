14e278bd67d904b6501c59af6e70a1871597d2ea
jihyeoniiiii <105184159+Jihyeoniiiii@users.noreply.github.com>
2024-10-10 18:31:43 +0900

    feat: add ShareMenuIcon to assets

---
+++ src/assets/icons/ShareMenuIcon.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShareMenuIcon: React.FC = () => (
+    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="20" height="20">
+        <circle cx="2" cy="2" r="2" />
+        <circle cx="2" cy="10" r="2" />
+        <circle cx="2" cy="18" r="2" />
+    </svg>
+);
+
+export default ShareMenuIcon;
